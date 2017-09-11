69c956612a061b4ea83fe520fa0b486f50447e7a
blake <ls745401341@outlook.com>
2017-09-11 18:30:41 +0800

    lishuai

---
+++ Template/WebRoot/success.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri="/struts-tags" prefix="s" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Success Page</title>
+</head>
+<body>
+    <h1>录入结果:</h1> <br/>
+    ${website}
+    <s:form action="quit" method="post">
+    <s:submit   value="返回" />
+    </s:form>
+</body>
+</html>
---
+++ Template/src/org/tjise/crawler/Website.java
@@ -0,0 +1,123 @@
+package org.tjise.crawler;
+
+import java.sql.Time;
+
+
+
+public class Website {
+	private int id;
+	private String name; //站点名字
+	private String channel; //频道信息
+	private String region;  //境内外
+	private String contry;  //国家名字
+	private String language; //语种
+	
+	
+	private Time start_time; //开始时间
+	private Time stop_time; //结束时间
+	
+	private String title; //文章标题的path
+	private String author; //文章作者的path
+	private String pubtime; //发布时间的path
+	private String content; //正文的path
+	private String source; //转发来源的path
+	private String Disable = "false";
+	
+	public String getDisable() {
+		return Disable;
+	}
+	public void setDisable(String disable) {
+		Disable = disable;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getChannel() {
+		return channel;
+	}
+	public void setChannel(String channel) {
+		this.channel = channel;
+	}
+	public Time getStart_time() {
+		return start_time;
+	}
+	public void setStart_time(Time start_time) {
+		this.start_time = start_time;
+	}
+	public Time getStop_time() {
+		return stop_time;
+	}
+	public void setStop_time(Time stop_time) {
+		this.stop_time = stop_time;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getPubtime() {
+		return pubtime;
+	}
+	public void setPubtime(String pubtime) {
+		this.pubtime = pubtime;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+	
+	
+	public String getRegion() {
+		return region;
+	}
+	public void setRegion(String region) {
+		this.region = region;
+	}
+	public String getContry() {
+		return contry;
+	}
+	public void setContry(String contry) {
+		this.contry = contry;
+	}
+	public String getLanguage() {
+		return language;
+	}
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+	@Override
+	public String toString() {
+		return "Website [id=" + id + ", name=" + name + ", channel=" + channel
+				+ ", region=" + region + ", contry=" + contry + ", language="
+				+ language + ", start_time=" + start_time + ", stop_time="
+				+ stop_time + ", title=" + title + ", author=" + author
+				+ ", pubtime=" + pubtime + ", content=" + content + ", source="
+				+ source + "]";
+	}
+	
+	
+}
